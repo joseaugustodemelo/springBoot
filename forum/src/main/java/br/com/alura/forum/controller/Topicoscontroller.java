fec16a5b8971b437376c5b54575298de53dbe0fa
Jose Augusto de Melo <joseaugusotdemelo@gmail.com>
2019-08-10 16:53:42 -0300

    Aula 04.01 - Trabalhando com POST.

--- forum/src/main/java/br/com/alura/forum/controller/Topicoscontroller.java
+++ forum/src/main/java/br/com/alura/forum/controller/Topicoscontroller.java
@@ -1,14 +1,17 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.controller.form.TopicoForm;
@@ -40,9 +43,11 @@ public class Topicoscontroller {
 
 	// @RequestMapping(value = "/topicos", method = RequestMethod.POST)
 	@PostMapping
-	public void cadastrar(@RequestBody TopicoForm form) {
-
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
 }
