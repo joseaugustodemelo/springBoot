f282fdc87a8806a39c5d868a34e58153413a0d68
Jose Augusto de Melo <joseaugusotdemelo@gmail.com>
2019-08-10 16:53:02 -0300

    Aula 4 -Trabalhando com POST.

--- forum/src/main/java/br/com/alura/forum/controller/Topicoscontroller.java
+++ forum/src/main/java/br/com/alura/forum/controller/Topicoscontroller.java
@@ -1,33 +1,48 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.model.Curso;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
+@RequestMapping("/topicos")
 public class Topicoscontroller {
 
 	@Autowired
 	private TopicoRepository topicoRepository;
 
-	@RequestMapping("/topicos")
+	@Autowired
+	private CursoRepository cursoRepository;
+
+	@GetMapping
 	public List<TopicoDto> lista(String nomeCurso) {
 
-		if (Objects.isNull(nomeCurso)) {			
+		if (Objects.isNull(nomeCurso)) {
 			List<Topico> topicos = topicoRepository.findAll();
 			return TopicoDto.converter(topicos);
 		} else {
 			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
 			return TopicoDto.converter(topicos);
 		}
+	}
+
+	// @RequestMapping(value = "/topicos", method = RequestMethod.POST)
+	@PostMapping
+	public void cadastrar(@RequestBody TopicoForm form) {
+
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
 	}
 }
--- forum/src/main/java/br/com/alura/forum/model/Topico.java
+++ forum/src/main/java/br/com/alura/forum/model/Topico.java
@@ -22,18 +22,28 @@ public class Topico {
 	private String titulo;
 	private String mensagem;
 	private LocalDateTime dataCriacao = LocalDateTime.now();
-	
+
 	@Enumerated(EnumType.STRING)
 	private StatusTopico status = StatusTopico.NAO_RESPONDIDO;
-	
+
 	@ManyToOne
 	private Usuario autor;
-	
+
 	@ManyToOne
 	private Curso curso;
-	
+
 	@OneToMany(mappedBy = "topico")
 	private List<Resposta> respostas = new ArrayList<>();
+
+	public Topico() {
+	}
+
+	public Topico(String titulo, String mensagem, Curso curso) {
+		super();
+		this.titulo = titulo;
+		this.mensagem = mensagem;
+		this.curso = curso;
+	}
 
 	@Override
 	public int hashCode() {
