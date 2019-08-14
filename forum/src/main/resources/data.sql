ce453e16c931019d8b3bd652a10225d5f14436a1
Jose Augusto de Melo <joseaugusotdemelo@gmail.com>
2019-08-13 21:08:53 -0300

    Aula 003 - Proteção com Spring Security

--- forum/src/main/resources/data.sql
+++ forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$mJwYDGPpGxFKCJL0j0uKa.PvKxWn.WSl/H9qb9h4cWV4TDYOGRAHO');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
